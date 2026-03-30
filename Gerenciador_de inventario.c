44f8cc67db6a3ef3aef39a9e5ba41c2a4064ed62
Lucas Miranda <104878497+LucasSMiranda@users.noreply.github.com>
2026-03-30 23:16:23 +0000

    Adicionado estoque mínimo e total

--- Gerenciador_de inventario.c
+++ Gerenciador_de inventario.c
@@ -4,7 +4,16 @@ int main()
 {
     char produtoA[30] = "Produto A";
     char produtoB[30] = "Produto B";
-    
+
+    unsigned int estoqueA = 1000;
+    unsigned int estoqueB = 2000;
+
+    unsigned int estoqueMinimoA = 500;
+    unsigned int estoqueminimoB = 2500;
+
+    double valorTotalA;
+    double valorTotalB;
+
 
 
 
