9448d63a1af9041244e2050e20c69c623f9d5118
Lucas Miranda <104878497+LucasSMiranda@users.noreply.github.com>
2026-03-30 23:30:04 +0000

    Ajuste incluindo valor A e B

--- Gerenciador_de inventario.c
+++ Gerenciador_de inventario.c
@@ -11,6 +11,9 @@ int main()
     unsigned int estoqueA = 1000;
     unsigned int estoqueB = 2000;
 
+    float valorA = 10.50;
+    float valorB = 20.40;
+
     unsigned int estoqueMinimoA = 500;
     unsigned int estoqueminimoB = 2500;
 
@@ -19,8 +22,8 @@ int main()
 
     //Exibir as informações dos produtos
 
-    printf("Produto %s tem estoque %u e o valor unitário é %f.", produtoA, estoqueA, valorA);
-    printf("Produto %s tem estoque %u e o valor unitário é %f.", produtoB, estoqueB, valorB);
+    printf("Produto %s tem estoque %u e o valor unitário é R$%.2f.\n", produtoA, estoqueA, valorA);
+    printf("Produto %s tem estoque %u e o valor unitário é R$%.2f.\n", produtoB, estoqueB, valorB);
 
 
 
